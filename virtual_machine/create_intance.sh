172a2f1b6c807f73757b991d4a86be8c23139a19
Pablo Ortiz de Galisteo Andres <portizdg@gmail.com>
2019-05-22 01:54:19 +0200

    b4 work

---
+++ virtual_machine/create_intance.sh
@@ -0,0 +1,18 @@
+export IMAGE_FAMILY="tf-latest-cpu"
+export ZONE="europe-west1-b"
+export INSTANCE_NAME="tfm"
+export INSTANCE_TYPE="n1-standard-1"
+gcloud compute instances create $INSTANCE_NAME \
+        --zone=$ZONE \
+        --image-family=$IMAGE_FAMILY \
+        --image-project=deeplearning-platform-release \
+        --maintenance-policy=TERMINATE \
+        --machine-type=$INSTANCE_TYPE \
+        --boot-disk-size=200GB \
+        --metadata-from-file startup-script=test.sh \
+        --metadata="install-nvidia-driver=True"
+
+gcloud compute firewall-rules create tfm --allow tcp:80
+gcloud compute firewall-rules create tfm --allow tcp:443
+gcloud compute instances create tfm --address 35.230.118.203
+
---
+++ virtual_machine/startup_script.sh
@@ -0,0 +1,30 @@
+sudo -su jupyter bash
+cd
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+/bin/bash Anaconda3-2019.03-Linux-x86_64.sh -b
+rm Anaconda3-2019.03-Linux-x86_64.sh
+
+
+conda install ipykernel
+ipython kernel install --name base --user
+
+
+
+pip install ipywidgets 
+
+jupyter nbextension enable --py widgetsnbextension
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+
+apt-get install openslide-tools	
+apt-get install python3-openslide
+pip install openslide-python
+pip install numpy==1.15.4 
+pip install pandas==0.23.4
+
+
+conda install tensorflow-gpu 
+conda install keras-gpu 
+conda install -c anaconda keras-gpu 
+
+# gcloud auth login
